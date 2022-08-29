0c14924124cdffe4e17915aa1172b50d10fd2478
Env-byte <tompenn18@gmail.com>
2022-08-29 19:34:03 +0100

    Added helper to check if object

--- index.js
+++ index.js
@@ -29,6 +29,14 @@ function render(resume) {
   });
 }
 
+Handlebars.registerHelper('ifObject', function(item, options) {
+  if(typeof item === "object") {
+    return options.fn(this);
+  } else {
+    return options.inverse(this);
+  }
+});
+
 module.exports = {
   render: render,
   pdfRenderOptions: {
